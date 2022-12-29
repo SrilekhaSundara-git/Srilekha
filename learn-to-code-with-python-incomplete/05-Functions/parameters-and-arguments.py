43a528419ee8d504260ae1fbd990b0b3743ac3b0
SrilekhaSundara <59963759+SrilekhaSundara-git@users.noreply.github.com>
2022-12-29 15:49:55 +0530

    functions-parameters-and-arguments

---
+++ learn-to-code-with-python-incomplete/05-Functions/parameters-and-arguments.py
@@ -0,0 +1,8 @@
+def p(params):
+    print(params)
+p('1')
+
+def add(param1, param2):
+    print('The sum of two numbers {} , {} is {}'.format(param1, param2, param1+param2))
+    return None
+add(1,2)
