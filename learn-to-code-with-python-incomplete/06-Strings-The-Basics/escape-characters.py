ac225cdaf84242c91e16122964753be5cbdd93c5
SrilekhaSundara <59963759+SrilekhaSundara-git@users.noreply.github.com>
2022-12-30 11:48:55 +0530

    escape characters

---
+++ learn-to-code-with-python-incomplete/06-Strings-The-Basics/escape-characters.py
@@ -0,0 +1,35 @@
+print("geronimo"[0:9:2])
+print("loquacious"[1:8:3])
+print("mendacious"[:10:4])
+print("hurricane"[::3])
+print("mastermind"[1::2])
+print("genuflect"[::-1])
+print("mastodon"[-5::2])
+print("paradise"[3:7])
+print("sirloin"[:])
+print("motorcycle"[2:6])
+print("retrograde"[8:])
+print("disco"[0:1])
+print("santacon"[5:15])
+print("hippocampus"[5:])
+print("motorbreath"[-6:])
+print("sanitarium"[-8:-5])
+print("potato"[-3:])
+print("genius"[:4])
+
+print("This will \nbegin on a \nnew line")
+print("\tOnce upon a time")
+print("\"To be or not to be\", said Hamlet")
+print('\'To be or not to be\', said Hamlet')
+print("Let's print a backslash: \\")
+file_name = r"C:\news\travel"
+print(file_name)
+some_random_number = 5
+some_obscure_calculation = 25
+some_additional_statistic_fetched_from_somewhere = 10
+final = some_random_number + \
+ some_obscure_calculation + \
+ some_additional_statistic_fetched_from_somewhere
+print(some_random_number,
+ some_obscure_calculation,
+ some_additional_statistic_fetched_from_somewhere)
